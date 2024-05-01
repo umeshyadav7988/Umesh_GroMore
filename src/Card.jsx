552e02c543665e43ea49b0fe42a48c07aba52c99
Umesh <97531471+umeshyadav7988@users.noreply.github.com>
2024-05-02 01:27:26 +0530

    Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,7 +9,7 @@ function Card({ title, imgsrc }) {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
-            Looking to kickstart your {title} journey affordably? Explore our budget-friendly {title} courses at Growmore! 
+            Looking to kickstart your {title} journey affordably? Explore our budget-friendly {title} courses at GrowMore! 
             </p>
             <NavLink to="/contact" className="btn btn-primary">
             Enroll
